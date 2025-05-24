4e44542c11260d2e36a006c574644db132a7ecf4
535230128 Felice Anastasia Lee <feliceanastasia25@gmail.com>
2025-05-24 15:30:20 +0700

    login 2

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StatController;
 use App\Http\Controllers\BarangKeluarController;
 
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
 // Redirect root to dashboard
 Route::get('/', function () {
     return redirect('/dashboard');
