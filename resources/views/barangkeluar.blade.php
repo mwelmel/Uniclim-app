e4ae5ee9dd9d32601e4bd0f1b3c20efae0a7d5a6
Maria Melisa H <mmelisaher@gmail.com>
2025-06-03 14:15:28 +0700

    perubahan

--- resources/views/barangkeluar.blade.php
+++ resources/views/barangkeluar.blade.php
@@ -15,10 +15,10 @@
     <div class="sidebar bg-dark text-white p-3" style="width: 220px; min-height: 100vh;">
       <img src="{{ asset('images/Logo UniCLim.png') }}" alt="UniClim Logo" class="img-fluid mb-4" style="max-width: 150px;" />
       <ul class="nav flex-column">
-        <li class="nav-item"><a class="nav-link active text-success" href="/dashboard">Dashboard</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="/dashboard">Dashboard</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/databarang">Data Barang</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/barangmasuk">Barang Masuk</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="/barangkeluar">Barang Keluar</a></li>
+        <li class="nav-item"><a class="nav-link active text-success" href="/barangkeluar">Barang Keluar</a></li>
         <hr class="bg-light" />
         <li class="nav-item"><a class="nav-link text-white" href="/account">Account</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/settings">Settings</a></li>
